556c034819c9e18b3a5bbf22fe2321a7dff65c9f
unknown <tomerpacific@gmail.com>
2020-10-04 09:30:47 +0300

    feature/rename-task creating an alert dialog service to present the dialog to rename activity/project/task

---
+++ app/src/main/java/com/example/projectprogresstracker/AlertDialogService.java
@@ -0,0 +1,44 @@
+package com.example.projectprogresstracker;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+class AlertDialogService {
+    private static final AlertDialogService ourInstance = new AlertDialogService();
+
+    static AlertDialogService getInstance() {
+        return ourInstance;
+    }
+
+    private AlertDialogService() {
+
+    }
+
+    public void showAlertDialogToRename(Context context, final TextView textViewToUpdate) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        final EditText editText = new EditText(context);
+        alert.setMessage("Enter the new name of the task");
+        alert.setTitle("Rename Current Task");
+
+        alert.setView(editText);
+
+        alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String text = editText.getText().toString();
+                textViewToUpdate.setText(text);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+
+        alert.show();
+    }
+}
